85606973a84bd6e089081ede9a3cd95fa74a9f5b
Ramakrishna P <ramakrishnareddy3090@gmail.com>
2026-01-31 18:44:52 +0530

    action on mongodb service2

--- mongodb.sh
+++ mongodb.sh
@@ -24,7 +24,7 @@ VALIDATE(){
     fi
 }
 
-cp mango.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Copying Mongodb Repo"
 
 dnf install mongodb-org -y &>>$LOGS_FILE
